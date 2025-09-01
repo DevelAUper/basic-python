c3015175560f996dd645491643720170022a2e81
DevelAUper <au803115@uni.au.dk>
2025-09-01 10:48:57 +0000

    Some of the exercises

--- src/pattern.py
+++ src/pattern.py
@@ -1,2 +1,12 @@
-
+n = 5
 # Print the pattern
+
+for i in range(n - 1):
+    for j in range (i):
+        print("*", end = " ")
+    print()
+
+for i in range(n - 1, 0, -1):
+    for j in range(i):
+        print("*", end=" ")
+    print()
